e102294fe3dc9e0a7b67f49daba38a23b3a21e39
Vaishnavi <vaishdw2207@gmail.com>
2021-01-11 22:24:57 +0530

    first commit

---
+++ MyApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "dwvicy";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, World! I\'m " + name);
+        }
+    }
+}
